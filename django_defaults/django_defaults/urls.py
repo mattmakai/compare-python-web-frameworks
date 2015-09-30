98a60f4c08651e3d2ef0c65ef2c30d4ef77f4960
Matthew Makai <matthew.makai@gmail.com>
2015-09-30 15:57:34 -0700

    initial app commit

--- django_defaults/django_defaults/urls.py
+++ django_defaults/django_defaults/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^mattmakai/', include(admin.site.urls)),
+    url(r'^', include('core.urls', namespace='core')),
 ]
